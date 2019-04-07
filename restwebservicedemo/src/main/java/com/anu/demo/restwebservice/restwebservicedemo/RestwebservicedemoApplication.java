0c987d2e221dc9f2764454b3bf57398545f7bdf7
Anupam Sharnagat <anupamsharnagat@gmail.com>
2019-04-07 15:47:34 +0200

    Initial commit

---
+++ restwebservicedemo/src/main/java/com/anu/demo/restwebservice/restwebservicedemo/RestwebservicedemoApplication.java
@@ -0,0 +1,13 @@
+package com.anu.demo.restwebservice.restwebservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebservicedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebservicedemoApplication.class, args);
+	}
+
+}
---
+++ restwebservicedemo/src/test/java/com/anu/demo/restwebservice/restwebservicedemo/RestwebservicedemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anu.demo.restwebservice.restwebservicedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestwebservicedemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
